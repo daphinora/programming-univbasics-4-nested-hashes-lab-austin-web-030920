3d8f20bc66d039afed1e4479dced54478e1a6b46
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-02-17 18:42:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -23,7 +23,7 @@ end
 
 def alan_kay_is_known_for
   
-programmer_hash[:alan_kay][:known_for]
+
 	
 	programmer_hash = 
  		{
@@ -39,7 +39,7 @@ programmer_hash[:alan_kay][:known_for]
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+     }programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
