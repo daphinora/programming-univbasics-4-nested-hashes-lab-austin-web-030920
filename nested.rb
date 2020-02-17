ca7e8f1861f16dc2e8b85ebdb9e6c920a7c687c0
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-02-17 18:45:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -53,7 +53,7 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
      }
   programmer_hash[:dennis_ritchie][:languages]
@@ -80,7 +80,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => "LISP", "C"}
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 
     
 end
